0253937d2b0a155e356ba5eb4eca8e6c3bf162d3
Franco Martin <main.francomartin@gmail.com>
2023-07-28 01:00:31 -0300

    Fix- Layout modal

--- src/components/InvoiceInformation/components/CreateInvoice.vue
+++ src/components/InvoiceInformation/components/CreateInvoice.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col gap-4 justify-start bg-[#ececec] rounded-lg shadow-md p-6 z-50 relative w-96"
+    class="flex flex-col gap-4 justify-start bg-[#ececec] rounded-lg shadow-md p-6 z-50 relative w-fit"
   >
     <div class="flex shrink-0 flex-row gap-4 justify-between text-[27px]">
       <h2 class="font-heading-bold text-Pallet-Purple capitalize">Create new invoice</h2>
@@ -15,7 +15,7 @@
       action=""
       @submit.prevent="handleClick"
       :v-model="formDataRef"
-      class="grid-cols-formFields gap-4 grid h-full w-full"
+      class="grid-cols-formFields gap-4 grid h-full w-128"
     >
       <template v-for="formData in formDataRef" :key="formData.id">
         <FormFields
@@ -27,16 +27,16 @@
           @update:value="(newValue) => (formData.value = newValue)"
         />
       </template>
-      <div class="flex flex-row gap-1 justify-start">
+      <div class="flex flex-row gap-2 justify-start items-end">
         <button
-          class="text-lg font-heading-bold px-2 py-1 rounded-md  bg-[#f7303060] hover:bg-[#f7303060] hover:border-[transparent] hover:shadow-lg"
+          class="text-lg h-fit w-fit font-heading-bold px-2 py-1 rounded-md border-[#f7303060] bg-[#f7303060] hover:bg-[#f7303060] hover:shadow-lg"
           type="button"
           @click="handleClick"
         >
           Cancel
         </button>
         <button
-          class="text-lg font-body-regular text-[#fff] px-2 py-1 rounded-md border-[transparent] border hover:border-Pallet-Purple bg-Pallet-Purple hover:border-[transparent] hover:bg-[#4308b1] hover:shadow-lg"
+          class="text-lg h-fit w-fit font-heading-bold text-[#fff] px-2 py-1 rounded-md hover:border-Pallet-Purple bg-Pallet-Purple hover:bg-[#4308b1] hover:shadow-lg"
           type="submit"
           @click="emitirEvento"
         >
--- src/components/InvoiceInformation/components/FormFields.vue
+++ src/components/InvoiceInformation/components/FormFields.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col gap-1 justify-start">
-    <label class="text-Head-UserGray font-body-regular text-[14px] capitalize" :for="name">{{
+    <label class="text-[#111] font-heading-bold text-base capitalize" :for="name">{{
       name
     }}</label>
     <input
       :type="inputType"
-      class="py-1 px-2 pl-3 rounded-md border border-Heading-Purple bg-[#e9e9e9]"
+      class="py-3 px-6 pl-3 rounded-md border border-Subtitle-Gray text-base bg-[#e9e9e9]"
       :name="name"
       :id="id"
       :value="value"
