a01f46dcb4d9a1e356fb7fdc2b502e6b2f13f031
Franco Martin <main.francomartin@gmail.com>
2023-07-28 00:45:32 -0300

    Feature - Add onclick modal

--- src/components/layout/LayoutNavbar.vue
+++ src/components/layout/LayoutNavbar.vue
@@ -8,27 +8,52 @@
         <SearchIcon></SearchIcon>
       </button>
       <input type="text" placeholder="Search Dashboard" class="bg-[#F5F9FD] h-[inherit]" />
-      <button class="m-auto" type="button">
+      <button
+        class="m-auto"
+        type="button"
+        @click="() => (optionToggle = !optionToggle)"
+        :class="optionToggle ? 'bg-[#dfdfff]' : ''"
+      >
         <OptionIcon></OptionIcon>
       </button>
     </nav>
-    <button type="button" class="w-[48px] h-[48px] rounded-[10px] bg-[#FEFEFF] shadow-md">
+    <button
+      type="button"
+      @click="() => (messageToggle = !messageToggle)"
+      class="w-[48px] h-[48px] rounded-[10px] bg-[#FEFEFF] shadow-md"
+      :class="messageToggle ? 'bg-[#dfdfff]' : ''"
+    >
       <MessageButtonIcon class="m-auto"></MessageButtonIcon>
     </button>
     <nav class="flex flex-row gap-4">
-      <button type="button" class="w-[48px] h-[48px] rounded-[10px] bg-[#FEFEFF] shadow-md">
+      <button
+        @click="() => (notificationToggle = !notificationToggle)"
+        type="button"
+        class="w-[48px] h-[48px] rounded-[10px] bg-[#FEFEFF] shadow-md"
+        :class="notificationToggle ? 'bg-[#dfdfff]' : ''"
+      >
         <NotificationIcon class="m-auto"></NotificationIcon>
       </button>
       <button
         type="button"
         class="w-[48px] h-[48px] rounded-[10px] bg-[#FEFEFF] shadow-md"
-        :onclick="handleToggle"
+        @click="() => (userTableToggle = !userTableToggle)"
+        :class="userTableToggle ? 'bg-[#dfdfff]' : ''"
       >
         <UserPersonIcon class="m-auto"></UserPersonIcon>
       </button>
     </nav>
   </div>
-  <div v-if="toggle" class="absolute top-24 right-[.35rem] w-[280px]">
+  <div v-if="userTableToggle" v-motion-fade class="absolute top-24 right-[.35rem] w-[280px]">
+    <UserTable />
+  </div>
+  <div v-if="notificationToggle" v-motion-fade class="absolute top-24 right-[5rem] w-[280px]">
+    <UserTable />
+  </div>
+  <div v-if="messageToggle" v-motion-fade class="absolute top-24 right-[10rem] w-[280px]">
+    <UserTable />
+  </div>
+  <div v-if="optionToggle" v-motion-fade class="absolute top-24 right-[25rem] w-[280px]">
     <UserTable />
   </div>
 </template>
@@ -42,8 +67,10 @@ import UserPersonIcon from '@/assets/icons/user-person.svg?component'
 import { ref } from 'vue'
 import UserTable from '../User/UserTable.vue'
 
-const toggle = ref<boolean>(false)
-const handleToggle = () => (toggle.value = !toggle.value)
+const userTableToggle = ref<boolean>(false)
+const optionToggle = ref<boolean>(false)
+const notificationToggle = ref<boolean>(false)
+const messageToggle = ref<boolean>(false)
 </script>
 
 <style scoped></style>
