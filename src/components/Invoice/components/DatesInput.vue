9771efa7869a5b8167d3b39acff1247e57142060
Franco Martin <main.francomartin@gmail.com>
2023-08-01 01:52:01 -0300

    Feature - Add invoice component

---
+++ src/components/Invoice/components/DatesInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <div
+        class="bg-[#ffffff] rounded-[5px] border-solid border-[rgba(151,151,151,0.50)] border pt-[5px] pr-5 pb-[5px] pl-5 flex flex-row gap-[100px] items-center justify-start shrink-0 relative">
+        <div class="flex flex-col gap-[5px] items-start justify-start shrink-0 relative">
+            <div class="text-[#979797] text-left relative" style="font: 400 12px 'Product Sans Medium', sans-serif">
+                {{ label }}
+            </div>
+
+            <input type="date" :placeholder="placeholder" class="text-[#000000] text-left relative"
+                style="font: 400 16px 'Product Sans Medium', sans-serif" />
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface definePropsData {
+    label: string
+    placeholder: string
+}
+defineProps<definePropsData>()
+
+</script>
+
+<style scoped></style>
--- src/views/PruebaView.vue
+++ src/views/PruebaView.vue
@@ -2,9 +2,24 @@
   <div class="example bg-slate-200 text-black">
     <h1>{{ $route.params.id }}</h1>
   </div>
-  <CampaingEarnings />
+  <div class="flex flex-row gap-4 w-full">
+    <ButtonMonths :title="'12 Month'" :variant="'focus'" />
+    <ButtonMonths :title="'12 Month'" :variant="''" />
+    <ButtonMonths :title="'12 Month'" :variant="''" />
+    <ButtonMonths :title="'12 Month'" :variant="''" />
+  </div>
+  <div class="flex flex-row gap-4 w-full">
+
+    <DatesInput label="End Date" placeholder="24/12/2024" />
+    <DatesInput label="End Date" placeholder="24/12/2024" />
+    <DatesInput label="End Date" placeholder="24/12/2024" />
+    <DocumentField/>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import CampaingEarnings from '@/components/StatsCard/components/CampaingEarnings.vue'
+import ButtonMonths from '@/components/Invoice/components/ButtonMonths.vue';
+import DatesInput from '@/components/Invoice/components/DatesInput.vue';
+import DocumentField from '@/components/Invoice/components/DocumentField.vue';
+
 </script>
